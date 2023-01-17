43b15f0b67288ba7b50a719ad505dddd156e2212
JeffyOLOLO <jeffyololo@gmail.com>
2023-01-17 23:37:14 +0100

    Refactoring of PCF8574

--- include/PCF8574.h
+++ include/PCF8574.h
@@ -9,7 +9,9 @@ BEGIN_AH_NAMESPACE
 class PCF8574 : public StaticSizeExtendedIOElement<8>
 {
 public:
-    PCF8574(uint8_t address = 0x20, pin_t interruptPin = NO_PIN)
+    constexpr static uint8_t BASE_ADDRESS = 0x20;
+
+    PCF8574(uint8_t address = BASE_ADDRESS, pin_t interruptPin = NO_PIN)
         : address(address), interruptPin(interruptPin) {}
 
     void pinModeBuffered(pin_t pin, PinMode_t mode) override;
@@ -24,18 +26,34 @@ public:
     void updateBufferedInputs() override;
     void updateBufferedPinModes();
 
+protected:
+    uint8_t readGPIO();
+    void writeGPIO(uint8_t data);
+
 private:
     uint8_t address;
     pin_t interruptPin;
 
-    // static bool isConnectedToI2C;
-
+    // mode of some pin(s) was changed
     bool pinModesDirty = true;
+    constexpr static bool PIN_MODE_INPUT  = 0;
+    constexpr static bool PIN_MODE_OUTPUT = 1;
+    // 0 - input; 1 - output
     BitArray<8> bufferedPinModes;
+
+    // pullup of some pin(s) was changed
     bool pullupsDirty = true;
+    constexpr static bool PULLUP_OFF = 0;
+    constexpr static bool PULLUP_ON  = 1;
+    // 0 - off; 1 - on
     BitArray<8> bufferedPullups;
+
+    // state of some pin(s) was changed
     bool outputsDirty = true;
+    // 0 - will set pin to low; 1 - will set pin to high
     BitArray<8> bufferedOutputs;
+
+    // state of pins is stored to this buffer
     uint8_t bufferedInputs = 0;
 };
 
--- src/PCF8574.cpp
+++ src/PCF8574.cpp
@@ -4,29 +4,39 @@
 
 BEGIN_AH_NAMESPACE
 
-// bool PCF8574::isConnectedToI2C = false;
-
 void PCF8574::pinModeBuffered(pin_t pin, PinMode_t mode) {
     if (mode == INPUT) {
-        pinModesDirty |= bufferedPinModes.get(pin) == 1;
-        pullupsDirty |= bufferedPullups.get(pin) == 1;
+        pinModesDirty |= bufferedPinModes.get(pin) == PIN_MODE_OUTPUT;
+        pullupsDirty |= bufferedPullups.get(pin) == PULLUP_ON;
         bufferedPinModes.clear(pin);
         bufferedPullups.clear(pin);
     } else if (mode == OUTPUT) {
-        pinModesDirty |= bufferedPinModes.get(pin) == 0;
+        pinModesDirty |= bufferedPinModes.get(pin) == PIN_MODE_INPUT;
         bufferedPinModes.set(pin);
     } else if (mode == INPUT_PULLUP) {
-        pinModesDirty |= bufferedPinModes.get(pin) == 1;
-        pullupsDirty |= bufferedPullups.get(pin) == 0;
+        pinModesDirty |= bufferedPinModes.get(pin) == PIN_MODE_OUTPUT;
+        pullupsDirty |= bufferedPullups.get(pin) == PULLUP_OFF;
         bufferedPinModes.clear(pin);
         bufferedPullups.set(pin);
     }
 }
 
+void PCF8574::begin() {
+    if (interruptPin != NO_PIN)
+        ExtIO::pinMode(interruptPin, INPUT_PULLUP);
+}
+
 void PCF8574::digitalWriteBuffered(pin_t pin, PinStatus_t status) {
-    bool boolstate = status == HIGH;
+    if (bufferedPinModes.get(pin) == PIN_MODE_INPUT)
+        // just get out of here if it's input pin
+        return;
+    const auto boolstate = status == HIGH;
     outputsDirty |= bufferedOutputs.get(pin) != boolstate;
     bufferedOutputs.set(pin, boolstate);
+}
+
+void PCF8574::analogWriteBuffered(pin_t pin, analog_t value) {
+    digitalWriteBuffered(pin, value >= 0x80 ? HIGH : LOW);
 }
 
 PinStatus_t PCF8574::digitalReadBuffered(pin_t pin) {
@@ -37,61 +47,57 @@ analog_t PCF8574::analogReadBuffered(pin_t pin) {
     return bitRead(bufferedInputs, pin) ? 1023 : 0;
 }
 
-void PCF8574::analogWriteBuffered(pin_t pin, analog_t value) {
-    digitalWriteBuffered(pin, value >= 0x80 ? HIGH : LOW);
-}
-
-void PCF8574::begin() {
-    if (interruptPin != NO_PIN)
-        ExtIO::pinMode(interruptPin, INPUT_PULLUP);
-    // I²C setup if necessary
-    // TODO: is this flag required?
-    //       init I²C in main.cpp
-    // if (!isConnectedToI2C)
-    // {
-    //     Wire.begin();
-    //     isConnectedToI2C = true;
-    // }
-}
-
+// Phisically update output pins state
 void PCF8574::updateBufferedOutputs() {
     updateBufferedPinModes();
     if (!outputsDirty)
+        // there is nothing to write to output pins
         return;
     uint8_t outputData = bufferedOutputs.getByte(0);
     // Write this byte over I²C
-    Wire.beginTransmission(address);
-    Wire.write(outputData);
-    Wire.endTransmission();
+    writeGPIO(outputData);
     outputsDirty = false;
 }
 
+// Physical read to the inputs buffer
 void PCF8574::updateBufferedInputs() {
+    updateBufferedPinModes();
     // Only update if a pin change interrupt happened
     if (interruptPin != NO_PIN && ExtIO::digitalRead(interruptPin) == HIGH)
         return;
-    Wire.beginTransmission(address);
-    bufferedInputs = Wire.read(); // read from I²C
-    Wire.endTransmission();
+    bufferedInputs = readGPIO(); // read from I²C
 }
 
+// Physical change pin modes if they were changed
 void PCF8574::updateBufferedPinModes() {
     if (pinModesDirty) {
-        uint8_t outputData = bufferedPinModes.getByte(0);
+        const auto outputData = bufferedPinModes.getByte(0);
         // Write this byte over I²C
-        Wire.beginTransmission(address);
-        Wire.write(outputData);
-        Wire.endTransmission();
+        writeGPIO(outputData);
         pinModesDirty = false;
     }
     if (pullupsDirty) {
-        uint8_t outputData = bufferedPullups.getByte(0);
+        const auto outputData = bufferedPullups.getByte(0);
         // Write this byte over I²C
-        Wire.beginTransmission(address);
-        Wire.write(outputData);
-        Wire.endTransmission();
+        writeGPIO(outputData);
         pullupsDirty = false;
     }
 }
+
+uint8_t PCF8574::readGPIO()
+{
+    Wire.beginTransmission(address);
+    const uint8_t data = Wire.read();
+    Wire.endTransmission();
+    return data;
+}
+
+void PCF8574::writeGPIO(uint8_t data)
+{
+    Wire.beginTransmission(address);
+    Wire.write(data);
+    Wire.endTransmission();
+}
+
 
 END_AH_NAMESPACE
